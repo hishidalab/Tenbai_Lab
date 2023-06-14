2fca573cbf84f5971c4f23b75351f45ed94c3fc4
Penguin0325 <fierce0728birds@gmail.com>
2023-06-14 20:40:42 +0900

    DB直した

---
+++ initdb/init.sql
@@ -0,0 +1,8 @@
+create database pengin;
+-- use pengin;
+-- CREATE TABLE IF NOT EXISTS userScertification (
+--     id INT PRIMARY KEY,
+--     name VARCHAR(50),
+--     email VARCHAR(100),
+--     certification VARCHAR(100)
+-- ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
